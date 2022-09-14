21f83c9b628ac3d61e7c5a5a70693a52a23b2dc4
waynewei <me@wayneclub.com>
2022-09-14 17:34:03 +0800

    init commit

--- 1480.running-sum-of-1-d-array.py
+++ 1480.running-sum-of-1-d-array.py
@@ -5,7 +5,10 @@
 #
 
 # @lc code=start
+from itertools import accumulate
+
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        return list(accumulate(nums))
 # @lc code=end
-
---
+++ 724.find-pivot-index.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=724 lang=python3
+#
+# [724] Find Pivot Index
+#
+
+# @lc code=start
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        total = sum(nums)
+        for i in range(len(nums)):
+            right_sum = total - left_sum - nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+        return -1
+# @lc code=end
