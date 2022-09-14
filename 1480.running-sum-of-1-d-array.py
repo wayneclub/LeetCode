1c2cc4529b9d06f7195814f12a81e897d9a2daf6
waynewei <me@wayneclub.com>
2022-09-14 17:22:07 +0800

    init commit

---
+++ 1480.running-sum-of-1-d-array.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1480 lang=python3
+#
+# [1480] Running Sum of 1d Array
+#
+
+# @lc code=start
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+# @lc code=end
+
