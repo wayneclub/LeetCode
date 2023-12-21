665345deebcff824ca8de76b897a9821b381a4ef
wayneclub <me@wayneclub.com>
2023-12-21 16:35:17 +0800

    update

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -11,9 +11,22 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """use difference to find the other number"""
-        for i, num in enumerate(nums):
-            if target - num in nums:
-                return [i, nums.index(target - num)]
+        # T: O(n^2)
+        # for i, num in enumerate(nums):
+        #     if target - num in nums[i + 1:]:
+        #         # find next index of the same number
+        #         return [i, nums.index(target - num, i+1)]
+
+        # T: O(n)
+        # use a map to store previous numbers
+        prev_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prev_map:
+                return [prev_map[diff], i]
+            prev_map[n] = i
 
 
+solution = Solution()
+print(solution.twoSum([3, 3], 6))
 # @lc code=end
